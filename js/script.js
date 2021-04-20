eaace33b65e4b5ac05b6c728e5b309b4d90127ba
eder medeiros <medeiroseder800@gmail.com>
2021-04-20 14:19:10 -0300

    Ponto inicial do projeto

---
+++ js/script.js
@@ -0,0 +1,73 @@
+//Variavel;
+
+var num1 = 5;
+var num2 = 7;
+
+var resultado;
+
+resultado = num1 = num2;
+
+document.write("Resultado : "+ resultado);
+
+resultado=resultado+5;
+
+document.write("<br>Resultado : "+resultado);
+
+
+
+
+if(resultado > 15){
+    document.write("<Maior que 15;")
+
+}else if(resultado == 15){
+    document.write("<br>Igual a 15");
+
+}else{
+    document.write("<br>Menor que 15");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
